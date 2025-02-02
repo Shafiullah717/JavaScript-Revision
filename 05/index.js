3faeb156e31fd5e1621187c5c24e8f877f235f31
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-02 17:39:08 +0500

    Learn Logical Operators

---
+++ 05/index.js
@@ -0,0 +1,30 @@
+console.log("Muhammad Shafiullah")
+// //Logical Operators
+// 1 And 
+// 2 Or 
+// 3 Not
+
+// let age = 5
+// let gender = "Male"
+
+// // if(age >= 20 && gender == "Male"){
+// //     console.log("You are adult now")
+// // }
+
+
+// let age = 20
+// let gender = "Female"
+
+// if(age >= 30 || gender == "Female"){
+//     console.log ("Expire")
+// }
+
+// Find Even and Odd number 
+
+let number = 6;
+if ( number % 2 != 0){
+    console.log("Odd Number")
+}
+else{
+    console.log("Even Number")
+}
