8a59afaf3142730e130bfb8e6e9aac8d40ac8780
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-02 14:46:58 +0500

    learn Conditional Statments

---
+++ 04/index.js
@@ -0,0 +1,61 @@
+console.log("Testing")
+// Conditional Statements
+
+// let age = 2
+// if(age >= 50){
+//     console.log("you are too old for vote casting")
+// }
+// else if (age >= 18){
+//     console.log("Yes you can vote:")
+// }
+// else{
+//     console.log("you are too young for vote casting")
+// }
+
+// Conditional Statements using ternary operator
+
+// let weight = 2002
+// weight >= 80 ? console.log("No") : console.log("Yes")
+
+// let weight = 22;
+
+// let Result = weight >= 50 ? "Yes" : "No"
+
+// console.log(Result)
+
+// Switch Conditions in JS
+
+// let Option = 1;
+
+// switch(Option){
+//     case 1 : console.log("Assalam o alikum")
+//     break
+//     case 2 : console.log("Hello!")
+//     break
+//     case 3 : console.log("Ni how")
+//     break
+//     default : console.log("Invalid Operation")
+// }
+
+let a = 22;
+let b = 44;
+let c = "*";
+
+switch(c){
+    case "+" : console.log(a + b)
+    break
+    case "-" : console.log(a - b)
+    break
+    case "*" : console.log(a * b)
+    break
+    case "/" : console.log(a / b)
+    break
+    default : console.log("invalid Operation")
+}
+
+
+
+
+
+
+
