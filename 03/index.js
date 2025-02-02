3a06c40b1274a4e312d7079c3f2c594938a94f6b
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-02 10:41:39 +0500

    Learn Js operators

---
+++ 03/index.js
@@ -0,0 +1,40 @@
+console.log("Muhammad Shafiullah")
+// Data Types in JavaScript
+// 1 number
+// 2 String
+// 3 Boolean
+// 4 null
+// 5 undefined
+// 6 object
+
+// Number
+
+let num1 = 33
+let num2 = 22
+// console.log(typeof(num1 + num2))
+
+// String
+
+let firstName = "Muhammad"
+let secandName = "Shafiullah"
+// console.log(firstName + ' ' + secandName)
+
+// Boolean
+let userLogIn = false
+// console.log(userLogIn)
+// if(0){
+//     console.log("Sucessfully")
+// }
+
+//Null and Undefined
+
+let myName = undefined
+// console.log(myName)
+
+//Objects
+const Studend={
+    firstName: "Muhammad",
+    lastName: "Cheema",
+    rollNumber: 44,
+}
+// console.log(Studend.rollNumber)
