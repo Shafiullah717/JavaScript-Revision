f64ed9d1561ce628888c0fbf91d108f7ec1bc3fa
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-03 11:36:33 +0500

    learn JS Loops

---
+++ 06/index.js
@@ -0,0 +1,50 @@
+console.log("Testing")
+// Conditions in JavaScript
+// For loop
+// while loop
+// do while loop
+
+// For loop
+// we use for loop when exectly know how many altration we want to do.
+// for(let i = 1; i<= 10; i++){
+//     console.log("Muhammad Shafiullah" , i)
+// }
+
+//While loop
+// we use while loop when donot know how many altration it may take.
+// let step = 50;
+// let home = 50;
+// while(step != home){
+//     step = step +1;
+//     console.log("Step taken", step)
+// }
+
+// Do-While loop
+// we use Do-while loop when we must want our program run atleast one.
+// let number = 1;
+// do{
+//     number = number +1
+//     console.log("Shafiullah")
+// }while(number <= 1)
+
+// simple game guess the input
+// let number = 50;
+// let guss = 0;
+// do{
+//     guss = parseInt(prompt("Guss the Number?"))
+//     if(guss == number){
+//         alert("You are Winner")
+//         break;
+//     }
+   
+// }while( guss != 0)
+
+
+
+
+
+
+
+
+
+
