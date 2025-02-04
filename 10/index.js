de2ec55c4cd806f8a552047cf167ecb4387607e9
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-04 17:50:53 +0500

    Currently learning arrays

---
+++ 10/index.js
@@ -0,0 +1,12 @@
+console.log("Arrays in JS")
+const Students = ["Shafiullah", "Ahsan", "Ahmad","waqas"]
+// console.log(Students.length)
+Students.push("Ibrahim")
+// console.log(Students)
+// Students.pop()
+Students.push("Javed")
+console.log(Students)
+Students.reverse()
+console.log(Students)
+const data =["Shafiullah","21","Single","Male"]
+console.log(data)
