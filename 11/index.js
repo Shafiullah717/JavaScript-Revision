9822d8b8c8a4bf09ae4d0c696570d8eff9fb476a
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-05 17:42:16 +0500

    Complete five tasks

--- 11/index.js
+++ 11/index.js
@@ -1,4 +1,4 @@
-window.console.log("Muhammad Shafiullah")
+// window.console.log("Muhammad Shafiullah")
 // console.log(window)
 // alert("Who the fuck you are?")
 // yourname = prompt("Please enter your name:", "xyz")
@@ -11,5 +11,9 @@ window.console.log("Muhammad Shafiullah")
 // document.title = "Shafiullah"
 // console.log(document.body)
 // document.write("HACKED")
-userName = prompt("Please enter your name")
-document.write("Hello" + " " + userName)
+// userName = prompt("Please enter your name")
+// document.write("Hello" + " " + userName)
+// const paragraph = document.querySelector('#name')
+// paragraph.innerHTML = "Ahsan"
+// console.log(paragraph.innerText)
+change_color = document.querySelectorAll(".box")
