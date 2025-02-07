ed8f54947db0cc01501e3670c8a5a18c99e0945e
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-07 21:32:03 +0500

    Complete Task one

---
+++ 12/index.js
@@ -0,0 +1,38 @@
+window.console.log("Muhammad Shafiullah")
+// // // // // // // // Task 01
+// const Blue_Color = document.getElementById("Name")
+// Blue_Color.style.color = "blue"
+// const Green_Color = document.querySelector(".paragraph")
+// console.log(Green_Color)
+// Green_Color.style.color = "purple"
+// // // // // // // // Task 02
+// const change_bg = document.getElementById("Name")
+// change_bg.style.backgroundColor = "gray"
+// const multi_by = document.querySelectorAll(".Box")
+// console.log(multi_by)
+// multi_by.forEach(item => {
+//     item.style.backgroundColor = "green"
+    
+// });
+// // // // /// // // // Task 03
+// const increaseFont = document.getElementById("Name")
+// increaseFont.style.fontSize = "60px"
+// const DecraseFont = document.querySelectorAll(".Box")
+// DecraseFont.forEach(item =>{
+//     item.style.fontSize = "4px"
+// })
+// // // // // // // // // // Task 04
+const Toggle = document.getElementById("Toggle")
+// console.log(Toggle)
+// Toggle.addEventListener("click" , )
+// const hide_Items = document.querySelectorAll(".Box1")
+// hide_Items.forEach(item =>{
+//     item.style.display = "none"
+// })
+// // // // // // // // // // Task 05
+
+
+
+
+
+
