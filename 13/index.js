8a36e74de20e5ed08935fd4949d3f12faaa2eb30
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-02-08 11:08:10 +0500

    Learn addEventListener

---
+++ 13/index.js
@@ -0,0 +1,47 @@
+console.log("EventListsners In JS")
+// function buttonclicked() {
+//     console.log("Button Clicked")
+  
+// }
+const Button = document.getElementById("ButtonClicked")
+// ButtonClicked.onclick = function (){
+//     console.log("button Clicked")
+// }
+
+// ButtonClicked.onclick = function (){
+//     console.log("I am fucked")
+// }
+
+// ButtonClicked.addEventListener("click", () =>{
+//     console.log("by adding event!")
+// })
+
+// function dosomething() {
+//     console.log("I declear out side the event")
+// }
+
+// const dosomething1 = function(){
+//     console.log("I also declear out side the event")
+// }
+
+// ButtonClicked.addEventListener("click", dosomething1)
+
+// ButtonClicked.onclick = function (){
+//     console.log("Button clicked")
+// }
+// const myNmae = document.getElementById("Name")
+// Button.addEventListener("click", function(){
+//     if(myNmae.style.color == "blue"){
+//         myNmae.style.color = "red"
+//     }
+//     else{
+//         myNmae.style.color = "blue"
+//     }
+// })
+
+// const body = document.querySelector('body').children
+// for(let i = 0; i < body.length; i++){
+//     body.item(i).addEventListener("click",function(){
+//         body.item(i).remove()
+//     })
+// }
